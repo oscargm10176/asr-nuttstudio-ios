6f7bb57175de037514ae1d0fb54b5aa9e292b37f
oscargm10176 <167119165+oscargm10176@users.noreply.github.com>
2026-02-22 19:53:03 -0600

    Initial Commit

---
+++ ASRTests/ASRTests.swift
@@ -0,0 +1,17 @@
+//
+//  ASRTests.swift
+//  ASRTests
+//
+//  Created by THRICE on 22/02/26.
+//
+
+import Testing
+@testable import ASR
+
+struct ASRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
